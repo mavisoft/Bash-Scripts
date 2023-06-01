1a361e3ce084ca661776237f6d39f47d400bd0d8
mavisoft <iyoboiyoha@gmail.com>
2023-06-01 09:54:54 +0100

    creating script

---
+++ PasswordGenerator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A simple password generator
+
+echo "This is a simple passwrod generator"
+read -p "Enter the length of the password: " PASS_LENGTH
+
+for p in $(seq 1);
+do
+	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+done
